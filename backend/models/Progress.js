41d7b11e37d3115c77f9137e2760d4ed2d4bff3b
Chloe Gavrilovic <chloe.gavv@gmail.com>
2024-09-19 00:12:02 -0400

    update Progress schema

--- backend/models/Progress.js
+++ backend/models/Progress.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const progressSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    taskId: { type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true },
+    taskId: { type: String, required: true },
     dateCompleted: { type: Date, default: Date.now },
     difficulty: { type: String, enum: ['beginner', 'intermediate', 'advanced'], required: true }
 });
