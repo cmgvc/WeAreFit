d6cc421c279f4ca792e828cfb327b8661ca833fa
Chloe Gavrilovic <chloe.gavv@gmail.com>
2024-10-08 08:58:51 -0400

    add logo to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import Logo from '../assets/WeAreFitLogo.svg';
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className='logo'>
-            <Link to="/">Home</Link>
-            {/* <img src={}></img> */}
+            <a href='/'>
+                <img src={Logo} alt='Home'></img>
+            </a>
         </div>
         <div className='nav'>
             <Link to="/dashboard">DASHBOARD</Link>
