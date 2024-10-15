5dfcdd44c2cbd57333eb6a29abcfff7165ec8a39
cmgvc <chloe.gavv@gmail.com>
2024-10-15 17:48:31 -0500

    fix backend port

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
 const cors = require('cors');
 const app   = express();
