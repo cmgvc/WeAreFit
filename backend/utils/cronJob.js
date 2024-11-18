984fb3958e8c1da80227f0ccc878d0ee1dff4274
cmgvc <chloe.gavv@gmail.com>
2024-11-17 21:11:04 -0500

    change cron job to run utc

--- backend/utils/cronJob.js
+++ backend/utils/cronJob.js
@@ -2,8 +2,11 @@ var cron = require('node-cron');
 const { generateDailyChallenge } = require('../controllers/challengeController');
 
 var task = cron.schedule('0 0 * * *', async () => {
-    await generateDailyChallenge()
+    await generateDailyChallenge();
     console.log('Daily challenge generated!');
+}, {
+    scheduled: true,
+    timezone: 'UTC' 
 });
 
 task.start();
